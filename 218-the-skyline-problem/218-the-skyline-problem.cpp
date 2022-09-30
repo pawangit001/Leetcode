1626e6072a7c3462d86ec8135eaac792bf1fc2a5
pawangit001 <103900468+pawangit001@users.noreply.github.com>
2022-10-01 01:39:04 +0530

    Time: 68 ms (43.61%), Space: 14.8 MB (73.95%) - LeetHub

---
+++ 218-the-skyline-problem/218-the-skyline-problem.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+  vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<pair<int, int>> edges;
+        int left, right, height;
+        for(int i=0; i<buildings.size(); i++){
+            left=buildings[i][0];
+            right=buildings[i][1];
+            height=buildings[i][2];
+            /*** make sure : for the same left point we use the bigger height ***/
+            edges.push_back(make_pair(left, -height));
+            edges.push_back(make_pair(right, height));
+        }
+        sort(edges.begin(), edges.end());
+        vector<vector<int>> result;
+        /*** use the multiset to store the max height util current pos ***/
+        multiset<int> m;
+        /*** left most height ***/
+        m.insert(0);
+        int pre=0, cur=0;
+        for(int i=0; i<edges.size(); i++){
+            pair<int, int> e=edges[i];
+            if(e.second < 0)  m.insert(-e.second);
+            else m.erase(m.find(e.second));
+            cur=*(m.rbegin());
+            if(cur!=pre){
+                result.push_back({e.first, cur});
+                pre=cur;
+            }
+        }
+        return result;  
+    }
+};
