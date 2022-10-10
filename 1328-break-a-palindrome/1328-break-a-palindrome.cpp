336998fa98806164d45cc60df82caf20c3ef7ffd
pawangit001 <103900468+pawangit001@users.noreply.github.com>
2022-10-10 12:58:05 +0530

    Time: 2 ms (51.67%), Space: 6.3 MB (45.53%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.cpp
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -4,11 +4,10 @@ public:
         int n=p.size();
         if(n==1)return "";
         
-   //     int t=26;
-        vector<int>ch(26,0);
-        for(int i=0;i<n;i++) ch[p[i]-'a']++;
-        if(p[0]!='a'){p[0]='a';return p;}
-        for(int i=1;i<n/2;i++)
+
+       
+       
+        for(int i=0;i<n/2;i++)
         {
            if(p[i]!='a')
            {
