513824ab3addd12038c8f6b395c07d9e246d696d
pawangit001 <103900468+pawangit001@users.noreply.github.com>
2022-11-06 16:11:18 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (74.71%) - LeetHub

---
+++ 0899-orderly-queue/0899-orderly-queue.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string orderlyQueue(string s, int k) {
+        if(k==1)
+        {
+            string ans=s;
+            s+=s;
+            for(int i=0;i<ans.size();i++)
+            {
+                    ans=min(ans,s.substr(i,ans.size()));
+            }
+            return ans;
+        }
+        else
+        {
+            sort(s.begin(),s.end());
+            return s;
+        }
+            
+    }
+};
