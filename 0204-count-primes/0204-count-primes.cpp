334815fc590c8465a303de1dc0b0ec54fa143820
pawangit001 <103900468+pawangit001@users.noreply.github.com>
2022-11-18 18:49:17 +0530

    Time: 237 ms (74.88%), Space: 10.2 MB (68.68%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -2,19 +2,21 @@ class Solution {
 public:
     int countPrimes(int n) {
         if(n<=2)return 0;
+        int ans=0;
         vector<bool>nPrime(n+1,true);
-        for(int i=2;i*i<=n;i++)
+        for(int i=2;i<n;i++)
         {
             if(nPrime[i]==true)
             {
-                for(int j=i*i;j<=n;j=j+i)
+                ans++;
+                for(int j=2*i;j<=n;j=j+i)
                 {
                     nPrime[j]=false;
                 }
             }
         }
-        int ans=0;
-        for(int i=2;i<n;i++)if(nPrime[i]==true)ans++;
+        // int ans=0;
+        // for(int i=2;i<n;i++)if(nPrime[i]==true)ans++;
         return ans;
     }
 };
